651c13342c9b56c2513f8fcd67cfe7927dfb3752
Alyson NGONYAMA <angonyam@e4r15p5.wethinkcode.co.za>
2018-11-10 10:43:30 +0200

    removed argc_argv - you can make your own

--- argc_argv/test_compile.sh
+++
@@ -1,1 +0,0 @@
-gcc -g -o testing cmd_options.c read_scan_list.c read_speedrun_number.c cmd_option_init.c count_params.c error_and_exit.c intostr.c read_port_list.c resolve_arguments.c arrayjoin.c read_file.c read_ip_cmd.c lib.a 
